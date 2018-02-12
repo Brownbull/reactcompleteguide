2eb8e06fe2cda8e6663dade4062e34d9c65267b8
Brownbull <carcamo.gabriel@gmail.com>
2018-02-12 19:58:18 -0300

    code working under ESLint

---
+++ first/.eslintrc.js
@@ -0,0 +1,45 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true,
+        "node": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaFeatures": {
+            "experimentalObjectRestSpread": true,
+            "jsx": true
+        },
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+        "no-unused-vars": [
+            "error", 
+            { 
+                // "vars": "all", 
+                "varsIgnorePattern": "[rR]eact", 
+                "args": "after-used", 
+                "ignoreRestSiblings": false 
+            }
+        ],
+        "indent": [
+            "error",
+            2
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "never"
+        ]
+    }
+};
