6b9898a4a80510f54ce4f486adea199252a41ef3
brownbull <carcamo.gabriel@gmail.com>
2018-02-21 10:27:42 -0300

    put componnets of first on first and delte all not needed on main folder

--- first/src/App.js
+++ first/src/App.js
@@ -5,20 +5,21 @@ import Person from "./components/Person/scenes/Person"
 class App extends Component {
   state = {
     persons: [
-      {name: "Gabriel", age:28},
-      {name: "Claudia", age:39},
-      {name: "Lia", age:25},
+      { id: "1", name: "Gabriel", age: 28 },
+      { id: "2", name: "Claudia", age: 39 },
+      { id: "3", name: "Lia", age: 26 },
     ],
-    otherState: "some other value"
+    otherState: "some other value",
+    showPersons: false
   } // eof state
 
   switchNameHandler = (newName) => {
     // don't do this this.state.persons.name[0].name = "Gabo"
     this.setState({
       persons: [
-        { name: newName, age: 28 },
-        { name: "Claudia", age: 39 },
-        { name: "Lia", age: 26 },
+        { id: "1", name: newName, age: 28 },
+        { id: "2", name: "Claudia", age: 39 },
+        { id: "3", name: "Lia", age: 26 },
       ]
     }) // eof this.setState
   } // eof switchNameHandler
@@ -32,8 +33,20 @@ class App extends Component {
       ]
     }) // eof this.setState
   } // eof nameChangeHandler
-  
-  render () {
+
+  deletePersonHandler = (personIndex) => {
+    //const persons = this.state.persons.slice() // slice will create a copy instead of use the reference method
+    const persons = [...this.state.persons] //this will copy instead of reference persons on state
+    persons.splice(personIndex, 1)  // this will delete element
+    this.setState({ persons: persons })
+  } // eof deletePersonHandler
+
+  togglePersonHandler = () => {
+    const doesShow = this.state.showPersons
+    this.setState({ showPersons: !doesShow })
+  } // eof togglePersonHandler
+
+  render() {
     const style = {
       backgroundColor: "white",
       font: "inherit",
@@ -42,23 +55,43 @@ class App extends Component {
       cursor: "pointer"
     }
 
+    let persons = null
+
+    // check state on each render and evaluate if show persons
+    if (this.state.showPersons) {
+      persons = (
+        <div >
+          {this.state.persons.map((person, key) => {
+            return (
+              <Person
+                key={person.id}
+                click={() => this.deletePersonHandler(key)}
+                name={person.name}
+                age={person.age}></Person>
+            ) // eof return
+          })}
+          {/* <Person
+            name={this.state.persons[0].name}
+            age={this.state.persons[0].age}>My hobbies: studying</Person>
+          <Person
+            name={this.state.persons[1].name}
+            age={this.state.persons[1].age}
+            click={this.switchNameHandler.bind(this, "Gabo2")}
+            change={this.nameChangeHandler}>Hobbies: Cooking</Person>
+          <Person
+            name={this.state.persons[2].name}
+            age={this.state.persons[2].age}></Person> */}
+        </div>
+      )
+    } // eof if (this.state.showPersons) 
+
     return (
       <div className="App">
-        <button 
+        <button
           style={style}
-          onClick={this.switchNameHandler.bind(this, "Gabo")}>Switch Name</button>
+          onClick={this.togglePersonHandler}>Toggle Persons</button>
         {/* <button onClick={() => this.switchNameHandler("Gabo1")}>Switch Name</button> */}
-        <Person 
-          name={this.state.persons[0].name} 
-          age={this.state.persons[0].age}>My hobbies: studying</Person>
-        <Person 
-          name={this.state.persons[1].name} 
-          age={this.state.persons[1].age}
-          click={this.switchNameHandler.bind(this, "Gabo2")}
-          change={this.nameChangeHandler}>Hobbies: Cooking</Person>
-        <Person 
-          name={this.state.persons[2].name} 
-          age={this.state.persons[2].age}></Person>
+        {persons}
       </div>
     ) // eof return
   } // eof render()
--- first/src/components/Person/scenes/Person.js
+++ first/src/components/Person/scenes/Person.js
@@ -5,10 +5,10 @@ const Person = (props) => {
   return (
     <div className="Person">
       <p onClick={props.click}>
-        I'm {props.name} and I'm {props.age} years old! 
+        I'm {props.name} and I'm {props.age} years old!
       </p>
       <p>{props.children}</p>
-      <input type="text" onChange={props.change} value={props.name}/>
+      <input type="text" onChange={props.change} defaultValue={props.name} />
     </div>
   ) //eof return
 } // eof const Person 
